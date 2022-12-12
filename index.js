6e47ab5dfcfbeaa06c8c1d28178e95b08f972600
Daisy-Violet <a.laska.yang@mail.ru>
2022-12-13 00:38:37 +0300

    Add new project

---
+++ index.js
@@ -0,0 +1,12 @@
+function firstClick() {
+    let first = document.getElementById("first");
+    first.style.background = 'red';
+}
+function secondClick() {
+    let second = document.getElementById("second");
+    second.style.background = 'green';
+}
+function thirdClick() {
+    let third = document.getElementById("third");
+    third.style.background = 'blue';
+}
